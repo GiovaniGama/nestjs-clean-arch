b1481452dec506e8b4095cf7e3ddb1af43655b8f
GiovaniGama <giovanigama.08@hotmail.com>
2023-12-07 07:49:38 -0300

    create service of env config

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+
+  constructor(private _configService: ConfigService) {}
+
+  getAppPort(): number {
+    return Number(this._configService.get<number>('PORT'));
+  }
+
+  getNodeEnv(): string {
+    return this._configService.get<string>('NODE_ENV');
+  }
+}
